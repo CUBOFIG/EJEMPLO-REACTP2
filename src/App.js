bb398945b76e15471690e04c34d8ba25e6561af9
Heriberto Figueroa Michel <hfigueroa@ucol.mx>
2020-03-09 23:32:52 -0600

    avances curso

--- src/App.js
+++ src/App.js
@@ -2,12 +2,34 @@ import React, {Fragment} from 'react';
 import "./styles/styles.scss"
 import Curso from './Curso'
 
+const cursos = [
+  {
+    nombre:"Curso De React",
+    price:"$200 MX",
+    profesor:"https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/Julian_Barnes_in_2019_02_%28cropped%29.jpg/640px-Julian_Barnes_in_2019_02_%28cropped%29.jpg",
+    nameProfesor:"Josse",
+    imagenCurso:"https://miro.medium.com/max/3150/0*p4oHn_Px5DokmoAr.png"
+  },{
+    nombre:"Curso Angular",
+    price:"$600 MX",
+    profesor:"https://observer.com/wp-content/uploads/sites/2/2020/01/elon-musk-twitter-advice.jpg?quality=80",
+    nameProfesor:"Manny",
+    imagenCurso:"https://muckibu.de/wp-content/uploads/2014/08/angular_bg1.png"
+  },{
+    nombre:"Curso VUE",
+    price:"$300 MX",
+    profesor:"https://observer.com/wp-content/uploads/sites/2/2020/01/elon-musk-twitter-advice.jpg?quality=80",
+    nameProfesor:"Manny",
+    imagenCurso:"https://programacion.net/files/article/20160316010348_vue-js.jpg"
+  }
+]
+
 const App = () => (
   <Fragment>
     <div className="main-banner img-container l-section" id="main-banner">
       <div className="ed-grid lg-grid-6">
         <div className="lg-cols-4 lg-x-2">
-          <img className="main-banner__img" alt="banner" src="https://i.pinimg.com/originals/86/69/ab/8669ababbeb05ed394d8aad8629a90e5.jpg"></img>
+          <img className="main-banner__img" alt="banner" src="https://image.freepik.com/foto-gratis/codigo-fuente-escritorio-papel-tapiz-lenguaje-computadora-codificacion-programacion_33771-600.jpg"></img>
           <div className="main-banner__data s-center">
             <p className="t2 s-mb-0">HERIBERTO FIGUEROA MICHEL</p>
             <p> ESTUDIANDO REACT</p>
@@ -18,9 +40,9 @@ const App = () => (
   </div>
 
   <div className="ed-grid m-grid-3">
-      <Curso />
-      <Curso />
-      <Curso />
+      {
+        cursos.map(curso => <Curso nombre={curso.nombre} price={curso.price} profesor={curso.profesor} nameProfesor={curso.nameProfesor} imagenCurso={curso.imagenCurso}/>)
+      }
   </div>
  </Fragment>
 )
--- src/Curso.jsx
+++ src/Curso.jsx
@@ -1,30 +1,63 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const Curso = () =>(
+/*const curso=[{
+    "nombre":"Curso De React", 
+    "price":"$200 MX", 
+    "profesor":"https://observer.com/wp-content/uploads/sites/2/2020/01/elon-musk-twitter-advice.jpg?quality=80",
+    "nameProfesor":"Manny",
+    "imagenCurso": "https://miro.medium.com/max/3150/0*p4oHn_Px5DokmoAr.png"
+  },
+  {
+    "nombre":"Curso VUE",
+    "price":"$300 MX",
+    "profesor":"https://observer.com/wp-content/uploads/sites/2/2020/01/elon-musk-twitter-advice.jpg?quality=80",
+    "nameProfesor":"Manny",
+    "imagenCurso": "https://programacion.net/files/article/20160316010348_vue-js.jpg"
+  }]*/
+
+const Curso = ({imagenCurso,nombre,profesor,nameProfesor,price}) =>(
     <article className="card">
       <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-        <img src="https://i.pinimg.com/originals/ef/63/f9/ef63f91d7ac356ba8bcc60c38f546d0a.jpg" alt="Poster de curso"></img>
+        <img src={imagenCurso} alt="Poster de curso"></img>
       </div>
       <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
         <h3 className="t5 s-mb-2 s-center">
-          Programación orientada a objetos con Go
+        {nombre}
         </h3>
         <div className="s-mb-2 s-main-center">
           <div className="card__teacher s-cross-center">
             <div className="card__avatar s-mr-1">
               <div className="circle img-container">
-                <img src="https://observer.com/wp-content/uploads/sites/2/2020/01/elon-musk-twitter-advice.jpg?quality=80" alt="Profesor"></img>
+                <img src={profesor} alt={nameProfesor}></img>
               </div>
             </div>
-            <span className="small">EL MANNY</span>
+            <span className="small">{nameProfesor}</span>
           </div>
         </div>
         <div className="s-main-center">
-          <a className="button--ghost-alert button--tiny" href="https://youtube.com">$ 20USD</a>
+          <a className="button--ghost-alert button--tiny" href="https://youtube.com">
+            {price}
+          </a>
         </div>
       </div>
     </article>
     
 )
+
+Curso.propTypes={
+  imagenCurso:  PropTypes.string,
+  nombre:       PropTypes.string,
+  profesor:     PropTypes.string,
+  nameProfesor: PropTypes.string,
+  price:        PropTypes.string
+}
+Curso.defaultProps={
+  imagenCurso: "https://videochums.com/article/switch-exclusive-indie-games-1.jpg",
+  nombre:       "Informacion no encontrada",
+  profesor:     "Informacion no encontrada",
+  nameProfesor: "Informacion no encontrada",
+  price:        "Informacion no encontrada",
+}
 
 export default Curso
